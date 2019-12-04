322ceeab029dcd4be564ebe2aa2aa84ba205ccea
Jiajian Zhang <handsomeJian@live.com>
2019-12-03 19:31:31 -0500

    Init

---
+++ Assets/Ship To Hell/LampMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampMove : MonoBehaviour
+{
+
+    public float height = 1.0f;
+    public float speed = 1.0f;
+
+
+    private float rad = 0.0f;
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rad = Random.value * 2 * Mathf.PI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rad += 0.01f * speed;
+        transform.position = startPos + new Vector3(0.0f, Mathf.Sin(rad) * height, 0.0f);
+    }
+}
